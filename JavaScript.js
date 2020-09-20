8f215584f11dde902772fc60d8f0498ee2f57310
Alexandros Nicolaou <alnicolaou96@gmail.com>
2020-09-19 23:02:09 -0400

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,19 @@
+﻿var day;
+//time api
+fetch("https://world-time2.p.rapidapi.com/ip", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "world-time2.p.rapidapi.com",
+        "x-rapidapi-key": "22f633b01amsh6be91bad6480fe2p14d4bdjsn2dd25817685a"
+    }
+})
+    .then(response => {
+        console.log(response);
+        return response.json();
+    })
+    .then(function (data) {
+        day = data.day_of_week;
+    })
+    .catch(err => {
+        console.log(err);
+    });
